1fe8a1b133f37ddddaf3b7157fd6674d9b2f6844
gil2rok <gilad.turok@gmail.com>
2023-03-06 16:00:17 -0500

    adding documentation to lle

--- hw2/code/locally_linear_embedding.py
+++ hw2/code/locally_linear_embedding.py
@@ -7,11 +7,23 @@ from scipy.sparse.linalg import eigsh
 from sklearn.neighbors import NearestNeighbors
 
 class LocallyLinearEmbeddings:
+    """ locally linear embedding
+    
+    """
     def __init__(self, n_neighbors, embedding_dim):
         self.n_neighbors = n_neighbors
         self.embedding_dim = embedding_dim
         
     def _numerical_stability(self, G):
+        """ increase numerical stability of gram matrix G
+
+        Args:
+            G ([n_neighbors x n_neighbors]): gram matrix
+
+        Returns:
+            G ([n_neighbors x n_neighbors]): numerically stable gram matrix
+        """
+        
         reg = 1e-3
         trace = np.trace(G)
         
@@ -24,13 +36,19 @@ class LocallyLinearEmbeddings:
         return G
         
     def _reconstruction_weights(self):
-        n = self.X.shape[0] # num data pts
-        W = lil_matrix(np.zeros((n,n))) # weight matrix [n x n]
+        """ compute reconstruction weights for each data point
+
+        Returns:
+            W ([n_samples x n_samples]): sparse weight matrix
+        """
+        
+        n_samples = self.X.shape[0] # num data pts
+        W = lil_matrix(np.zeros((n_samples,n_samples))) # weight matrix [n_samples x n_samples]
         
         # nearest neighbors estimator
         nbrs_est = NearestNeighbors(n_neighbors=self.n_neighbors + 1).fit(self.X)
         
-        for i in range(n):
+        for i in range(n_samples):
             xi = self.X[i:i+1] # i-th data pt, xi [1 x D]
             nbrs_idx = nbrs_est.kneighbors(xi, return_distance=False).squeeze() # nearest neighbors idx
             nbrs_idx = nbrs_idx[1:] # remove self
@@ -47,21 +65,41 @@ class LocallyLinearEmbeddings:
         return W
     
     def _embedding(self):
-        n = self.X.shape[0] # num data pts
-        I = lil_matrix(np.eye(n)) # sparse identity matrix [n x n]
-        M = (I - self.W).T @ (I - self.W) # [n x n]
+        """ compute lower dimensional embedding
+
+        Returns:
+            Y ([n_samples x embedding dim]): embedding
+        """
+        n_samples = self.X.shape[0] # num data pts
+        I = lil_matrix(np.eye(n_samples)) # sparse identity matrix [n_samples x n_samples]
+        M = (I - self.W).T @ (I - self.W) # [n_samples x n_samples]
         _, eig_vec = eigsh(M, k=self.embedding_dim + 1, which='SM')
-        return eig_vec[:, 1:] # remove first eigenvector [n x embedding_dim]
+        Y = eig_vec[:, 1:] # remove first eigenvector [n_samples x embedding_dim]
+        return Y
         
     def fit_transform(self, X):
-        self.X = X # data matrix [n x D]
-        
-        self.W = self._reconstruction_weights() # sparse weight matrix [n x n]
-        return self._embedding() # embedding [n x embedding_dim]
+        """ fit model and return embedding
 
-    def fit(self, X, d):
+        Args:
+            X ([n_samples x D]): data matrix
+
+        Returns:
+            Y ([n_samples x embedding_dim]): embedding of data matrix
+        """
+        
+        self.X = X # data matrix [n x D]
+        self.W = self._reconstruction_weights() # sparse weight matrix [n_samples x n_samples]
+        Y = self._embedding() # embedding [n_samples x embedding_dim]
+        return Y
+
+    def fit(self, X):
+        """ fit model
+
+        Args:
+            X ([n_samples x D]): data matrix
+
+        """
         self.fit_transform(X)
-        return self
         
         
         
